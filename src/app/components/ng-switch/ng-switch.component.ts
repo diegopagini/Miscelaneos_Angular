b450c3193fcbd6f98acb3ad73142872c3726f96c
Diego Pagini <diegopagini777@gmail.com>
2020-12-30 15:50:17 -0300

    ngSwitch

---
+++ src/app/components/ng-switch/ng-switch.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch',
+  templateUrl: './ng-switch.component.html',
+  styleUrls: ['./ng-switch.component.css'],
+})
+export class NgSwitchComponent implements OnInit {
+  alerta: string = 'warning';
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
