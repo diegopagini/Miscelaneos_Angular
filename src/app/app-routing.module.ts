508b553602e317b3b62d0eb8a91d6a95cfa2d2a4
Diego Pagini <diegopagini777@gmail.com>
2020-12-31 06:30:59 -0300

    rutas hijas,es bastante facil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'usuario', component: UsuarioComponent },
+  {
+    path: 'usuario',
+    component: UsuarioComponent,
+    children: [
+      { path: 'nuevo', component: UsuarioNuevoComponent },
+      { path: 'detalle', component: UsuarioDetalleComponent },
+      { path: 'editar', component: UsuarioEditarComponent },
+    ],
+  },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
 
